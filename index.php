fbf940d697b068bd8cb48b2d8ded7739aac681e1
Nhat Nguyen Khac <khacnhat.info@gmail.com>
2017-07-18 18:00:28 +0700

    Fix time zone

--- index.php
+++ index.php
@@ -15,11 +15,13 @@ class Picture{
     }
 }
 
+date_default_timezone_set('UTC');
+
 $objs = array(
-    new Picture("Costa Brava", "1.png", date_default_timezone_set("Y/M/d")),
-    new Picture("In memory of Nelson Mandela", "2.png", date_default_timezone_set("Y/M/d")),
-    new Picture("South of Serbia III", "3.png", date_default_timezone_set("Y/M/d")),
-    new Picture("Mr and Mrs Happy and their son, Smiler", "4.png", date_default_timezone_set("Y/M/d")),
+    new Picture("Costa Brava", "1.png", date("Y/M/d")),
+    new Picture("In memory of Nelson Mandela", "2.png", date("Y/M/d")),
+    new Picture("South of Serbia III", "3.png", date("Y/M/d")),
+    new Picture("Mr and Mrs Happy and their son, Smiler", "4.png", date("Y/M/d")),
 );
 
 echo json_encode($objs);
