db57bbfaad15060c9fbc76cad14ed3ff37471ba5
Nhat Nguyen Khac <khacnhat.info@gmail.com>
2017-07-18 17:53:52 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+class Picture{
+    public $name;
+    public $url;
+    public $date;
+
+    public function __construct($name, $url, $date)
+    {
+        $this->name = $name;
+        $this->url = $url;
+        $this->date = $date;
+    }
+}
+
+$objs = array(
+    new Picture("Costa Brava", "1.png", date_default_timezone_set("Y/M/d")),
+    new Picture("In memory of Nelson Mandela", "2.png", date_default_timezone_set("Y/M/d")),
+    new Picture("South of Serbia III", "3.png", date_default_timezone_set("Y/M/d")),
+    new Picture("Mr and Mrs Happy and their son, Smiler", "4.png", date_default_timezone_set("Y/M/d")),
+);
+
+echo json_encode($objs);
+?>
